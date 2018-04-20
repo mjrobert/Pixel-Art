0294f3568037423ce5cd1cf6793e0cc00a8b7028
Matthew Robert <mjrobert@gmail.com>
2018-04-20 17:17:30 +0400

    added variables

--- designs.js
+++ designs.js
@@ -1,5 +1,24 @@
 // Select color input
+
+const colorPicker = document.querySelector('input[type="color"]');
+
+let color = colorPicker.value;
+
+colorPicker.addEventListener("input", function () {
+    color = colorPicker.value;
+});
+
+
+
+
+
 // Select size input
+
+let height = document.getElementById("inputHeight").value;
+let width = document.getElementById("inputWidth").value;
+
+const submit = document.querySelector('input[type="submit"]');
+
 
 // When size is submitted by the user, call makeGrid()
 
