760f3beb145179b4d34cb2fd0f6d58089b392ec9
Matthew Robert <mjrobert@gmail.com>
2018-04-20 17:44:11 +0400

    submit button prevent default

--- designs.js
+++ designs.js
@@ -4,23 +4,27 @@ const colorPicker = document.querySelector('input[type="color"]');
 
 let color = colorPicker.value;
 
-colorPicker.addEventListener("input", function () {
+colorPicker.addEventListener('input', function () {
     color = colorPicker.value;
 });
-
-
-
 
 
 // Select size input
 
 let height = document.getElementById("inputHeight").value;
-let width = document.getElementById("inputWidth").value;
+let width = document.getElementById('inputWidth').value;
 
 const submit = document.querySelector('input[type="submit"]');
 
 
 // When size is submitted by the user, call makeGrid()
+
+submit.addEventListener('click', function(event){
+  event.preventDefault(); //so it doesn't reload when i click submit
+  makeGrid()
+});
+
+
 
 function makeGrid() {
 
